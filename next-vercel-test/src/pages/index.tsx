28a3c8d36808dbeef6efdacf498991722f7ea1fe
Daniel Sundman <daniel@yds.se>
2020-06-17 22:03:29 +0200

    env test

--- next-vercel-test/src/pages/index.tsx
+++ next-vercel-test/src/pages/index.tsx
@@ -6,6 +6,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Welcome to Expo + Next.js 👋 - Now TypeScript...and some more text.</Text>
+      <Text>process.env.NEXT_PUBLIC_TEST_PARAM: "{process.env.NEXT_PUBLIC_TEST_PARAM}"</Text>
+      <Text>process.env.TEST_PARAM: "{process.env.TEST_PARAM}"</Text>
     </View>
   );
 }
