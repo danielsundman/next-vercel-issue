c4b5477a9f97fc5bdbec103089f1b5834076c4b9
Daniel Sundman <daniel@yds.se>
2020-06-17 22:07:19 +0200

    env test

--- next-vercel-test/src/pages/index.tsx
+++ next-vercel-test/src/pages/index.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Welcome to Expo + Next.js 👋 - Now TypeScript...and some more text...and another try!</Text>
+      <Text style={styles.text}>Welcome to Expo + Next.js 👋 - Now TypeScript...and some more text...and another try! +</Text>
       <Text>process.env.NEXT_PUBLIC_TEST_PARAM: "{process.env.NEXT_PUBLIC_TEST_PARAM}"</Text>
       <Text>process.env.TEST_PARAM: "{process.env.TEST_PARAM}"</Text>
     </View>
